ce24bb4f4f0cd958d46aff891f2b32a7e8f0cfb4
Milobo <dinhminhx3@gmail.com>
2019-08-03 10:52:51 +0700

    ViewPagerAndroid
    ViewPagerAndroid

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const {
   ScrollView,
   Platform,
   StyleSheet,
-  ViewPagerAndroid,
   InteractionManager,
 } = ReactNative;
 const TimerMixin = require('react-timer-mixin');
+imported ViewPagerAndroid from '@react-native-community/viewpager'
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
